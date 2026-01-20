495af96578c30a106cdb993e998e2c9234f62e8e
LeanneCodes <leannegoldsmith95@yahoo.com>
2026-01-20 10:57:16 +0000

    add a history router and add location model file

--- server/routers/locationRoutes.js
+++ server/routers/locationRoutes.js
@@ -4,6 +4,7 @@ const locationController = require('../controllers/locationController');
 const locationRouter = Router();
 
 locationRouter.get('/location', locationController.getLocation);
+locationRouter.get('/location/history', locationController.getLocationHistory);
 locationRouter.post('/location', locationController.setLocation);
 locationRouter.patch('/location', locationController.updateLocation);
 locationRouter.get('/location/validate', locationController.validateLocation);
